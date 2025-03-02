5655bb71b71fd490db4ef19a9077dbe5b03883a5
anukritich <chyanukriti@gmail.com>
2025-03-02 17:00:34 +0530

    PersonalStudy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Dashboard from "./pages/Dashboard";
 import JoinGroup from "./pages/JoinGroup";
 import CreateGroup from "./pages/CreateGroup";
 import StudyGroupChat from "./pages/StudyGroupChat";
-import LandingPage from "./pages/LandingPage"; // Ensure this is correctly imported
+import LandingPage from "./pages/LandingPage"; 
+import PersonalStudyRoom from "./pages/PersonalStudyRoom";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Route path="/join-group" element={<JoinGroup />} />
       <Route path="/create-group" element={<CreateGroup />} />
       <Route path="/study-group/:groupId" element={<StudyGroupChat />} />
+      <Route path="/personal-study-room" element={<PersonalStudyRoom />} />
     </Routes>
   );
 }
